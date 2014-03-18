ccc8d3a1aa4b4f2e3b166bb786f206d592b2a910
Jeremy Rodi <redjazz96@gmail.com>
2014-03-18 17:29:47 -0400

    Sprockets 2.x

--- lib/sprockets/liquidscript_template.rb
+++ lib/sprockets/liquidscript_template.rb
@@ -1,13 +1,22 @@
 require "liquidscript"
 require "sprockets"
+require "tilt"
 
 module Sprockets
-  class LiquidscriptTemplate < Template
-    def self.default_mime_type
-      'application/javascript'
+  class LiquidscriptTemplate < Tilt::Template
+    self.default_mime_type = 'application/javascript'
+    
+    def self.engine_initialized?
+      defined? ::Liquidscript
     end
+    
+    def initialize_engine
+      require_template_library "liquidscript"
+    end
+    
+    def prepare; end
 
-    def render(context)
+    def evaluate(scope, locals, &block)
       @output ||= Liquidscript.compile(data)
     end
   end
