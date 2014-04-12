9c802bb74fb6f1420ec4833e51d7c61621903bbf
Mechazawa <Mega@IOException.at>
2014-04-12 02:28:58 +0200

    Added syntax check and return error code 1 on compile/syntax failure

--- lib/liquidscript/cli.rb
+++ lib/liquidscript/cli.rb
@@ -23,6 +23,26 @@ module Liquidscript
         puts "COMPILING: #{file}"
         perform_compiliation(file,
           options[:out] || file.gsub('.liq', '.js'))
+      end 
+    end
+
+    desc "syntax FILES", "Syntax check given file"
+    long_desc <<-LONGDESC
+     It will run a syntax check on the file listed. If the file does
+     not pass the syntax check it will return the error code 1. If 
+     the file give is - it will check the standard input.
+    LONGDESC
+    def syntax(*files)
+      errors = 0
+      files.each do |file|
+        print "CHECKING: #{file} "
+        if !preform_syntax_check(file)
+          errors += 1
+        end
+      end
+      if errors > 0
+        puts "#{errors} file(s) did not pass the syntax check"
+        exit 1
       end
     end
 
@@ -45,9 +65,31 @@ module Liquidscript
       outfile.write(out)
     rescue StandardError => e
         $stderr.puts "ERROR: #{e.class}: #{e.message}"
+
         $stderr.puts e.backtrace[0..5].map { |s| "\t#{s.gsub(/^.*?\/lib\/liquidscript\//, "")}" }.join("\n")
+        exit 1
     ensure
       ([infile, outfile] - [$stdin, $stdout]).each(&:close)
+    end
+
+    def preform_syntax_check(file)
+      infile = if file == '-'
+        $stdin
+      else
+        File.open(file, "r")
+      end
+
+      Liquidscript.compile(infile.read)
+      puts "OK"
+      return true
+    rescue StandardError => e
+        puts "FAIL"
+        $stderr.puts "ERROR: #{e.class}: #{e.message}"
+
+        $stderr.puts e.backtrace[0..5].map { |s| "\t#{s.gsub(/^.*?\/lib\/liquidscript\//, "")}" }.join("\n")
+        return false
+    ensure
+      ([infile] - [$stdin]).each(&:close)
     end
   end
 end
